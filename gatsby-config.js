a85292f15002965b819975f8e0b9ad7d8516a20d
Nicholas Duffy <duffn@outlook.com>
2020-03-20 06:52:05 -0600

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-service-relief`,
+      options: {
+        authorName: `@amber1ey`,
+        authorLink: `https://twitter.com/amber1ey`,
+        city: `Denver`,
+        state: `Colorado`,
+      },
+    },
+  ],
+}
