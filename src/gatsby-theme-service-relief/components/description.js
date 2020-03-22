835781c4485195d11d7a709bb9c93730c8fb30c8
Nicholas Duffy <duffn@outlook.com>
2020-03-22 07:56:22 -0600

    Update description wording

--- src/gatsby-theme-service-relief/components/description.js
+++ src/gatsby-theme-service-relief/components/description.js
@@ -4,7 +4,7 @@ const Description = ({ city }) => {
   return (
     <p className="text-lg mb-8">
       A directory of fundraisers for {city} restaurants, bars, local businesses,
-      organzations, and more that can use our support during mandated shutdowns.
+      organzations, and more that can use our support during closures.
     </p>
   )
 }
